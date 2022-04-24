8b7bab51d4a42026455d97daf42a0a336062902b
OH SW <ohseungo@naver.com>
2022-04-24 22:41:27 +0900

    다른 방식 추가

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import "./App.css";
 import { useSelector, useDispatch, connect } from "react-redux";
 import { increment, decrement, input } from "./redux/action";
 
-function App({ onIncrement, onDecrement, onInput, counter }) {
-  // const dispatch = useDispatch(); //props 에서 받아오는 것도 가능
-  // const handleIncrement = () => {
-  //   dispatch(increment());
-  // };
-  // const handleDecrement = () => {
-  //   dispatch(decrement());
-  // };
+function App({ counter }) {
+  const dispatch = useDispatch(); //props 에서 받아오는 것도 가능 props.dispatch
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   const [newCounter, setNewCounter] = useState(0);
   const handleChange = (e) => {
     setNewCounter(e.target.value);
-    onInput(e.target.value);
+    dispatch(input(e.target.value));
   };
   return (
     <div className="app">
       <h1 className="counter">{counter}</h1>
-      <button className="counterButton" onClick={onIncrement}>
+      <button className="counterButton" onClick={handleIncrement}>
         +
       </button>
-      <button className="counterButton" onClick={onDecrement}>
+      <button className="counterButton" onClick={handleDecrement}>
         -
       </button>
       <input
@@ -34,19 +35,53 @@ function App({ onIncrement, onDecrement, onInput, counter }) {
     </div>
   );
 }
-// Map Redux state to React Component props
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-// Map dispatch function to React Component props
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onIncrement: () => dispatch(increment()),
-    onDecrement: () => dispatch(decrement()),
-    onInput: (newValue) => dispatch(input(newValue)),
-  };
-};
+const mapStateToProps = (state) => state;
+export default connect(mapStateToProps)(App);
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// function App({ onIncrement, onDecrement, onInput, counter }) {
+//   // const dispatch = useDispatch(); //props 에서 받아오는 것도 가능
+//   // const handleIncrement = () => {
+//   //   dispatch(increment());
+//   // };
+//   // const handleDecrement = () => {
+//   //   dispatch(decrement());
+//   // };
+//   const [newCounter, setNewCounter] = useState(0);
+//   const handleChange = (e) => {
+//     setNewCounter(e.target.value);
+//     onInput(e.target.value);
+//   };
+//   return (
+//     <div className="app">
+//       <h1 className="counter">{counter}</h1>
+//       <button className="counterButton" onClick={onIncrement}>
+//         +
+//       </button>
+//       <button className="counterButton" onClick={onDecrement}>
+//         -
+//       </button>
+//       <input
+//         className="newInput"
+//         name="newNumber"
+//         value={newCounter}
+//         onChange={handleChange}
+//       />
+//     </div>
+//   );
+// }
+// // Map Redux state to React Component props
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+// // Map dispatch function to React Component props
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     onIncrement: () => dispatch(increment()),
+//     onDecrement: () => dispatch(decrement()),
+//     onInput: (newValue) => dispatch(input(newValue)),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
